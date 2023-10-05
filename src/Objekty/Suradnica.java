0df85d305f96ea86b6449b5048843e0c494a42a9
NikolajCupan <cupannikolaj@gmail.com>
2023-10-05 12:38:52 +0200

    Presun konstant

--- src/Objekty/Suradnica.java
+++ src/Objekty/Suradnica.java
@@ -1,16 +1,13 @@
 package Objekty;
 
+import Ostatne.Konstanty;
+
 public class Suradnica
 {
     public static final char VYCHOD = 'V';
     public static final char ZAPAD = 'Z';
     public static final char SEVER = 'S';
     public static final char JUH = 'J';
-
-    public static final double DLZKA_MIN = -180;
-    public static final double DLZKA_MAX = 180;
-    public static final double SIRKA_MIN = -90;
-    public static final double SIRKA_MAX = 90;
 
     private double poziciaDlzky;
     private double poziciaSirky;
@@ -39,7 +36,7 @@ public class Suradnica
 
     private void validujDlzku(double poziciaDlzky)
     {
-        if (poziciaDlzky < DLZKA_MIN || poziciaDlzky > DLZKA_MAX)
+        if (poziciaDlzky < Konstanty.DLZKA_MIN || poziciaDlzky > Konstanty.DLZKA_MAX)
         {
             throw new IllegalArgumentException("Nespravna pozicia dlzky, musi byt z rozsahu <-180; 180>!");
         }
@@ -47,7 +44,7 @@ public class Suradnica
 
     private void validujSirku(double poziciaSirky)
     {
-        if (poziciaSirky < SIRKA_MIN || poziciaSirky > SIRKA_MAX)
+        if (poziciaSirky < Konstanty.SIRKA_MIN || poziciaSirky > Konstanty.SIRKA_MAX)
         {
             throw new IllegalArgumentException("Nespravna pozicia sirky, musi byt z rozsahu <-90; 90>!");
         }
---
+++ src/Ostatne/Konstanty.java
@@ -0,0 +1,9 @@
+package Ostatne;
+
+public class Konstanty
+{
+    public static final double DLZKA_MIN = -180;
+    public static final double DLZKA_MAX = 180;
+    public static final double SIRKA_MIN = -90;
+    public static final double SIRKA_MAX = 90;
+}
